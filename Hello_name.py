d379d13406b0b8d32f1dc70aa8e80cc34e5160ac
Amit-Me <97550088+Amit-Me@users.noreply.github.com>
2022-03-19 19:26:35 +0000

    Add files via upload

---
+++ Hello_name.py
@@ -0,0 +1,3 @@
+if __name__ == "_main_":
+    name = input("Enter your name : ")
+    print("Hello "+name)
--- callMessageToFriend.py
+++ callMessageToFriend.py
@@ -1,7 +1,11 @@
 import pyautogui as pg
 import time
-time.sleep(10)
-
-for i in range(10):
-    pg.write("call")
-    pg.press('Enter')
+#time.sleep(10)
+pg.write("firefox")
+pg.press('Enter')
+pg.write("hello")
+pg.press('Enter')
+#for i in range(10):
+#    pg.write("mozilla")
+#    pg.write("call")
+#    pg.press('Enter')
